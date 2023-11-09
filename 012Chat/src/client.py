daa27b623c756b2510f6d3df44384a213ea64ef9
guumkie <149574317+guumkie@users.noreply.github.com>
2023-11-09 12:40:20 +0900

    Update client.py

--- 012Chat/src/client.py
+++ 012Chat/src/client.py
@@ -16,13 +16,14 @@ def connect_to_server(HOST, PORT):
 
 #서버 데이터 받기
 def receive():
-    global c_socket, decoded_data
+    global c_socket, decoded_data, data, sender, dataTime
     while True:
         try:
             recvData = c_socket.recv(1024)
             if not recvData: 
                 break
             decoded_data = recvData.decode('utf-8', errors='ignore')
+            data, sender, dataTime = decoded_data.split(' ', 2) #받은 데이터 각 변수로 분리
             print(decoded_data)  #데이터 수신 시 출력 (디버깅용)
 
         except Exception as e:
@@ -35,7 +36,7 @@ def receive():
 #메시지, 입력 시간 보내기
 def send_message(message):
     sendTime = time.strftime('%Y/%m/%d %H:%M:%S')  # 입력시간
-    c_socket.send((f'{message}  {sendTime}').encode('utf-8'))
+    c_socket.send((f'{message} {sendTime}').encode('utf-8'))
 
 #서버 연결 종료
 def close_connection():
