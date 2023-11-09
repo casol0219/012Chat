25c7c4aa37a1b56e85bec3ef4ede9317b29eba06
guumkie <149574317+guumkie@users.noreply.github.com>
2023-11-09 11:00:09 +0900

    Add files via upload

---
+++ 012Chat/src/client.py
@@ -0,0 +1,42 @@
+import socket
+import time
+from _thread import *
+import threading
+
+decoded_data = ""
+c_socket = None
+
+#서버 연결
+def connect_to_server(HOST, PORT):
+    global c_socket
+    c_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    c_socket.connect((HOST, PORT))
+
+    threading.Thread(target=receive).start()
+
+#서버 데이터 받기
+def receive():
+    global c_socket, decoded_data
+    while True:
+        try:
+            recvData = c_socket.recv(1024)
+            if not recvData: 
+                break
+            decoded_data = recvData.decode('utf-8', errors='ignore')
+            print(decoded_data)  #데이터 수신 시 출력 (디버깅용)
+
+        except Exception as e:
+            print(f"데이터 수신 중 예외 발생: {e}")
+            break
+        
+    close_connection()  # 연결 종료
+
+
+#메시지, 입력 시간 보내기
+def send_message(message):
+    sendTime = time.strftime('%Y/%m/%d %H:%M:%S')  # 입력시간
+    c_socket.send((f'{message}  {sendTime}').encode('utf-8'))
+
+#서버 연결 종료
+def close_connection():
+    c_socket.close()
---
+++ 012Chat/src/server.py
@@ -0,0 +1,74 @@
+import socket
+from _thread import *
+
+c_list = []
+forbidden_words = ["시발", "미친", "꺼져", "놈"]
+
+def changeWord(message):
+    for word in forbidden_words:
+        if word in message:
+            replacement = '@' * len(word)   #금칙어에 걸린 문자열의 길이만큼 '@'로 대체
+            message = message.replace(word, replacement)
+    return message
+
+def groupChat(c_socket, addr):
+    sender_info = f"{addr[0]}:{addr[1]}"  #발신자 정보
+    print(f">> {sender_info} 님이 입장하셨습니다.")
+    
+    while True:
+        try:
+            data = c_socket.recv(1024)
+            if not data:
+                print(f">> {sender_info} 님이 대화방을 나갔습니다.")
+                break
+            
+            decoded_data = data.decode('utf-8', errors='ignore')  #인코딩된 데이터를 디코딩하고, 에러 발생 시 무시
+
+            # 금칙어 처리
+            filtered_data = changeWord(decoded_data)
+            print(sender_info, '-', decoded_data)  #오가는 메시지들 로깅
+            
+            for client in c_list:
+                if client != c_socket:
+                    client.send(filtered_data.encode('utf-8'))
+
+                    
+        except ConnectionResetError as e:
+            print(f">> {sender_info} 님이 대화방을 나갔습니다.")
+            for client in c_list:
+                if client != c_socket:
+                    client.send(f">> {sender_info} 님이 대화방을 나갔습니다.".encode('utf-8'))
+            break
+                            
+    if c_socket in c_list:
+        c_list.remove(c_socket)
+        print('>> 접속자 목록을 갱신했습니다 - 접속자 수 : ', len(c_list))
+        print(c_list)
+        
+    c_socket.close()
+
+
+def start_server():
+    PORT = 12346
+    s_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s_socket.bind(('', PORT))
+    s_socket.listen()
+
+    print('>> 012 Chat Server Start')
+    print('>> 접속자를 기다리는 중...\n')
+
+    try:
+        while True:
+            c_socket, addr = s_socket.accept()
+            c_list.append(c_socket)
+            print("접속자 수 : ", len(c_list))
+            start_new_thread(groupChat, (c_socket, addr))
+
+    except Exception as e:
+        print('에러 발생:', e)
+    finally:
+        s_socket.close()
+
+
+if __name__ == "__main__":
+    start_server()
