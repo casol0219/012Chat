4d22b0edcae8974c963378b46ee304354fba3dfa
guumkie <149574317+guumkie@users.noreply.github.com>
2023-11-10 14:28:39 +0900

    Update client.py

--- 012Chat/src/client.py
+++ 012Chat/src/client.py
@@ -1,39 +1,36 @@
 import socket
 import time
 from _thread import *
-import threading
-
-decoded_data = ""
-c_socket = None
-
-# 서버 연결
-def connect_to_server(HOST, PORT):
-    global c_socket
-    c_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    c_socket.connect((HOST, PORT))
-
-    receive_thread = threading.Thread(target=receive)
-    receive_thread.daemon = True  #쓰레드를 데몬으로 설정
-    receive_thread.start()
 
 #서버 데이터 받기
-def receive():
-    global c_socket, decoded_data, data, sender, dataTime
+def receive(c_socket):
+    global decoded_data
     while True:
         try:
             recvData = c_socket.recv(1024)
-            if not recvData: 
+            if not recvData:
+                print("연결이 종료되었습니다.")
                 break
-            decoded_data = recvData.decode('utf-8', errors='ignore')
+            decoded_data = recvData.decode('utf-8')
             print(decoded_data)  #데이터 수신 시 출력 (디버깅용)
-            data, sender, dataTime = decoded_data.split(' ', 2) #받은 데이터 각 변수로 분리
-
         except Exception as e:
-            print(f"데이터 수신 중 예외 발생: {e}")
+            print(f"예외가 발생했습니다: {e}")
             break
-        
-    close_connection()  # 연결 종료
 
+# 서버 연결
+def connect_to_server():
+    global c_socket
+    HOST = "127.0.0.1"
+    PORT = 12346
+
+    c_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    c_socket.connect((HOST, PORT))
+
+    start_new_thread(receive, (c_socket,))
+
+    #receive_thread = threading.Thread(target=receive)
+    #receive_thread.daemon = True  #쓰레드를 데몬으로 설정
+    #receive_thread.start()
 
 #메시지, 입력 시간 보내기
 def send_message(message):
