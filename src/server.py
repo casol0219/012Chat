a6fb22c9fcb8fe678cccbea0f9befc94c9ac4f48
guumkie <149574317+guumkie@users.noreply.github.com>
2023-11-12 01:33:26 +0900

    Update server.py

--- src/server.py
+++ src/server.py
@@ -32,17 +32,21 @@ def groupChat(c_socket, addr):
         try:
             data = c_socket.recv(1024).decode('utf-8')
             if "NICKNAMECHANGE" != data.split("::")[0]:
-                sendTime, recvMessage = data.split(' ', 1)    #받은 메시지, 보낸 시간 분리
-            
+                if '**' in data:
+                    sendTime, recvMessage = data.split('**', 1)    #받은 메시지, 보낸 시간 분리
+                
             if not data:
                 print(f">> {nickname} 님이 대화방을 나갔습니다.")
                 break
+            
             #닉네임 변경 start
             elif "NICKNAMECHANGE" == data.split("::")[0]:
                 tmp_nick = data.split("::")[1]
+                
                 if tmp_nick in c_name and tmp_nick == nickname:
                     c_socket.send("NICKNAMECHANGE::FALSE".encode('utf8'))
                     c_name.remove(nickname)
+                    
                     nickname = c_socket.recv(1024).decode('utf8').split("::")[-1]
                 else: c_socket.send("NICKNAMECHANGE::TRUE".encode('utf8'))
                 c_name.append(nickname)
@@ -65,21 +69,26 @@ def groupChat(c_socket, addr):
                     recipient_socket=c_connections[c_socket]
                     c_socket.send(f"1:1 대화가 수락되었습니다.".encode())
                     recipient_socket.send(f"1:1 대화가 수락되었습니다.".encode())
+                    
                 elif recvMessage.lower()=='n':
                     c_socket.send(f"1:1 대화 요청이 거부되었습니다.".encode())
                     recipient_socket.send(f"1:1 대화 요청이 거부되었습니다.".encode())
                     del c_connections[c_socket]
+                    
                 else:
                     recipient_socket.send(f"{nickname}: {recvMessage}".encode())
 
             #일반 채팅
             else:
-                filtered_data = changeWord(recvMessage)
-                print(f"{nickname} - {recvMessage}  {sendTime}")  #오가는 메시지들 로깅
-                
+                #금칙어 처리
+                filtered_data = (changeWord(recvMessage).encode('utf-8')).decode('utf-8')
+                #오가는 메시지들 로깅
+                print(f"{nickname} - {recvMessage}  {sendTime}")
+
+                #자신 포함 모든 접속자에게 메시지 전송
                 for client in c_list:
-                    if client != c_socket:
-                        client.send(filtered_data.encode('utf-8'))
+                    #발신자 정보, 시간, 금칙어 처리 메시지 보내기
+                    client.send(f'{nickname}**{sendTime}**{filtered_data}'.encode('utf-8'))
                     
         except ConnectionResetError as e:
             print(f">> {nickname} 님이 대화방을 나갔습니다.")
