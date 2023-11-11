9c1addfef264bf100bcf48ca0ff933b9f5ee9c12
guumkie <149574317+guumkie@users.noreply.github.com>
2023-11-12 01:30:35 +0900

    Update client.py

--- src/client.py
+++ src/client.py
@@ -3,8 +3,8 @@ import time
 from _thread import *
 
 #서버 데이터 받기
-def receive(c_socket,window):
-    global decoded_data
+def receive(c_socket, window, callback):
+    global nickname, datetime
     while True:
         try:
             recvData = c_socket.recv(1024)
@@ -19,13 +19,18 @@ def receive(c_socket,window):
                 print("성공")
                 change_nick(window,0)
 
-            print(decoded_data)  #데이터 수신 시 출력 (디버깅용)
+            display_text = ""
+            if '**' in decoded_data:
+                nickname, datetime, data = decoded_data.split('**', 2)
+                display_text = f"{nickname}  {datetime}\n{data}"
+                callback(display_text)
+
         except Exception as e:
             print(f"예외가 발생했습니다: {e}")
             break
 
 # 서버 연결
-def connect_to_server(window):
+def connect_to_server(window, callback):
     global c_socket
     HOST = "127.0.0.1"
     PORT = 12346
@@ -33,11 +38,7 @@ def connect_to_server(window):
     c_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     c_socket.connect((HOST, PORT))
 
-    start_new_thread(receive, (c_socket,window))
-
-    #receive_thread = threading.Thread(target=receive)
-    #receive_thread.daemon = True  #쓰레드를 데몬으로 설정
-    #receive_thread.start()
+    start_new_thread(receive, (c_socket, window, callback))
 
 def my_portNum():
     return str(c_socket.getsockname()[1])
@@ -62,7 +63,7 @@ def change_nick(window,flag):
 #메시지, 입력 시간 보내기
 def send_message(message):
     sendTime = time.strftime('%Y/%m/%d %H:%M:%S')  # 입력시간
-    c_socket.send((f'{message} {sendTime}').encode('utf-8'))
+    c_socket.send((f'{sendTime}**{message}').encode('utf-8'))
 
 #서버 연결 종료
 def close_connection():
