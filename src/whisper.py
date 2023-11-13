610a701ab999d648e3ab83004710989e6ffc0b56
casol0219 <103026465+casol0219@users.noreply.github.com>
2023-11-13 17:46:39 +0900

    Feat: Update whisper.py

---
+++ src/whisper.py
@@ -0,0 +1,18 @@
+from changeWord import *
+
+def whisper(recvMessage,sendTime,c_name,c_list,nickname,c_socket):
+    recipient_name=recvMessage.split(' ')[1]
+    print(f"{recipient_name}")
+    whisper_message=' '.join(recvMessage.split(' ')[2:])
+    filtered_data = changeWord(whisper_message)
+    print(c_name)
+
+    if recipient_name in c_name:
+        recipient_idx=c_name.index(recipient_name)
+        r_socket=c_list[recipient_idx]
+        r_socket.send(f"{nickname}님으로부터의 귓속말: {filtered_data}".encode('utf-8'))
+    else:
+        c_socket.send(f"존재하지 않는 사용자입니다.")
+
+    #로깅
+    print(f"[whisper] {nickname} to {recipient_name} - {recvMessage}  {sendTime}")
