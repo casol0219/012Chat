3f91b6d0d08b344c04ca4b8e966d590ab00e8814
casol0219 <103026465+casol0219@users.noreply.github.com>
2023-11-14 22:29:02 +0900

    Update whisper.py

--- src/whisper.py
+++ src/whisper.py
@@ -12,7 +12,7 @@ def whisper(recvMessage,sendTime,c_name,c_list,nickname,c_socket):
         r_socket=c_list[recipient_idx]
         r_socket.send(f"{nickname}님으로부터의 귓속말: {filtered_data}".encode('utf-8'))
     else:
-        c_socket.send(f"존재하지 않는 사용자입니다.")
+        c_socket.send(f"상대방이 존재하지 않습니다.".encode('utf-8'))
 
     #로깅
     print(f"[whisper] {nickname} to {recipient_name} - {recvMessage}  {sendTime}")
