3cf2e48b7650c9a7d52e44cfd8882b970cbf85f5
Saenozu <dawn1504@naver.com>
2023-11-19 00:21:28 +0900

    bytes header (change nick & userlist)

--- src/server.py
+++ src/server.py
@@ -37,7 +37,7 @@ def groupChat(c_socket, addr):
         try:
             data = c_socket.recv(1024)
             if not data.startswith(bytes_header):
-                if '**' in data:
+                if '**'.encode('utf-8') in data:
                     sendTime, recvMessage = data.decode('utf-8').split('**', 1)    #받은 메시지, 보낸 시간 분리
                     msg_bytes=recvMessage.encode('utf-8')
                 
